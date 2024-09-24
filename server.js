59a1e7712ad1f0d9fa6e692f240d12704cf25838
GuilhermeVDG <guivdouradog@gmail.com>
2024-09-23 22:13:49 -0300

    feat: Adicionando cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const port = process.env.PORT || 3122;
 const httpCodesRoutes = require('./routes/http');
+
+app.use(cors());
 
 app.use(express.static('public'));
 
