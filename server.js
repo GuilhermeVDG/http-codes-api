571cc72cded8925776a2f183f99dd7e98226b954
Bruna Cruzl <brunavascruz@gmail.com>
2024-09-29 16:12:21 -0300

    feat: Ajustando o path das imagens

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3122;
 const httpCodesRoutes = require('./routes/http');
+const path = require('path');
 
 app.use(cors());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', httpCodesRoutes);
 
